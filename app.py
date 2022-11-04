ebab150c2ca85c854e10618f86f83f385f2abc77
joehutt <sneakerhutt@gmail.com>
2022-11-04 07:24:08 -0400

    Adafruit Added + Shopify Additions

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setupUi(self)
         self.show()
     def setupUi(self, MainWindow):
-        self.version ='1.06'
+        self.version ='1.07'
         MainWindow.setFixedSize(1109, 600)
         MainWindow.setStyleSheet("background-color: #1E1E1E;")
         MainWindow.setWindowTitle(f"Pi Bot - Version {self.version}")
