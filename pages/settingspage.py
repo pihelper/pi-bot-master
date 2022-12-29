b2a80a81c1f2f0de759a21679f072c74127b2524
joehutt <sneakerhutt@gmail.com>
2022-12-29 06:12:08 -0500

    Notification changes

--- pages/settingspage.py
+++ pages/settingspage.py
@@ -188,7 +188,7 @@ class SettingsPage(QtWidgets.QWidget):
     def test_notif(self):
         current_os = platform.system().lower()
         if 'linux' in current_os:
-            os.system('notify-send "' + '[Pi Bot] Test Notification' + '" "' + 'This is a test notification!' + '"')
+            s.call('notify-send "' + '[Pi Bot] Test Notification' + '" "' + 'This is a test notification!' + '"')
         elif 'darwin' in current_os:
             mac_notify('[Pi Bot] Test Notification', "This is a test notification!")
         elif 'window' in current_os:
