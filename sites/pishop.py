e2ed4424c274adbcab6b7d905a8df5572a7c28a0
joehutt <sneakerhutt@gmail.com>
2023-03-14 10:26:13 -0400

    Added PiShop CA support

--- sites/pishop.py
+++ sites/pishop.py
@@ -1,12 +1,14 @@
 import json
 import random
 import time
+from threading import Thread
+
 import requests
 from bs4 import BeautifulSoup
 import utils
+from harvester import Harvester
 from utils import send_notif, return_data
-from webhook import good_web, failed_web, cart_web
-
+from webhook import new_web
 
 def get_csrf(html):
     ind = html.index("csrf_token")
@@ -55,8 +57,9 @@ class PiShop:
         self.verification_token = ''
         self.cosignment_id = ''
         self.title = ''
-        if 'pishop.us' not in str(self.info).lower():
-            self.status_signal.emit({"msg": "Invalid PiShop.us Link", "status": "error"})
+        self.site_suffix = 'ca' if 'pishop.ca' in str(self.info).lower() else 'us' if 'pishop.us' in str(self.info).lower() else 'INVALID'
+        if self.site_suffix == 'INVALID':
+            self.status_signal.emit({"msg": "Invalid PiShop Link", "status": "error"})
         elif not self.invalid_captcha_key():
             self.main_site = self.info
             self.status_signal.emit({"msg": "Starting", "status": "normal"})
@@ -85,7 +88,7 @@ class PiShop:
                     if self.pid == '':
                         self.pid = soup.find_all(attrs={"name": "product_id"})[0]['value']
                     if self.title == '':
-                        self.title = str(soup.find('title').get_text()).replace(' - PiShop.us','').split(' - CM4')[0]
+                        self.title = str(soup.find('title').get_text()).replace(' - PiShop','').split(' - CM4')[0]
                         self.product_signal.emit(f'{self.title} [{self.qty}]')
                     stock_json = check_stock(get_item_page.text)
                     if self.csrf_token == '':
@@ -93,17 +96,19 @@ class PiShop:
                     if stock_json['product_attributes']['instock']:
                         atc_data = {'action': 'add', 'product_id': self.pid, 'qty[]': self.qty}
                         self.status_signal.emit({"msg": "Adding to cart", "status": "normal"})
-                        cart_req = self.session.post('https://www.pishop.us/remote/v1/cart/add',data=atc_data).json()['data']
+                        cart_req = self.session.post(f'https://www.pishop.{self.site_suffix}/remote/v1/cart/add',data=atc_data).json()['data']
                         if 'error' in cart_req:
                             self.status_signal.emit({"msg": "OOS on cart", "status": "error"})
                             time.sleep(float(self.error_delay))
                         else:
                             self.status_signal.emit({"msg": "Added to cart", "status": "carted"})
+                            self.start_time = time.time()
                             self.cart_id = cart_req['cart_id']
                             self.item_id = cart_req['cart_item']['id']
                             self.image = cart_req['cart_item']['thumbnail']
                             if self.settings['webhookcart']:
-                                cart_web(self.info, self.image,'PiShop (US)',self.title,self.profile['profile_name'])
+                                new_web('carted', f"PiShop ({self.site_suffix.upper()})", self.image, self.title, self.profile['profile_name'])
+                                #cart_web(self.info, self.image,f'PiShop ({self.site_suffix.upper()})',self.title,self.profile['profile_name'])
                         return
                     else:
                         self.status_signal.emit({"msg": "Waiting for restock", "status": "monitoring"})
@@ -115,12 +120,12 @@ class PiShop:
                 time.sleep(float(self.error_delay))
 
     def get_checkout_page(self):
-        check_headers = {'authority': 'www.pishop.us', 'host': 'www.pishop.us', 'scheme': 'https',
+        check_headers = {'authority': f'www.pishop.{self.site_suffix}', 'host': f'www.pishop.{self.site_suffix}', 'scheme': 'https',
                          'accept': 'application/vnd.bc.v1+json', 'accept-encoding': 'gzip, deflate',
                          'accept-language': 'en-US,en;q=0.9', 'content-type': 'application/json',
                          'X-XSRF-TOKEN': self.session.cookies.get_dict()['XSRF-TOKEN'],
-                         'x-checkout-sdk-version': '1.269.0', 'referer': "https://www.pishop.us/checkout",
-                         'origin': 'https://www.pishop.us',
+                         'x-checkout-sdk-version': '1.269.0', 'referer': f"https://www.pishop.{self.site_suffix}/checkout",
+                         'origin': f'https://www.pishop.{self.site_suffix}',
                          'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
                          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
                          'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"', 'Sec-Fetch-Site': 'same-origin',
@@ -130,7 +135,7 @@ class PiShop:
         while True:
             try:
                 self.status_signal.emit({"msg": "Getting checkout", "status": "normal"})
-                checkout_req = self.session.get('https://www.pishop.us/checkout',headers=check_headers)
+                checkout_req = self.session.get(f'https://www.pishop.{self.site_suffix}/checkout',headers=check_headers)
                 if checkout_req.url.endswith('/checkout'):
                     return
                 else:
@@ -142,11 +147,11 @@ class PiShop:
                 time.sleep(float(self.error_delay))
 
     def submit_email(self):
-        email_url = f'https://www.pishop.us/api/storefront/checkouts/{self.cart_id}/billing-address?include=cart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
+        email_url = f'https://www.pishop.{self.site_suffix}/api/storefront/checkouts/{self.cart_id}/billing-address?include=cart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
         email_data = {'email': self.profile['shipping_email']}
         email_headers = {
-            'authority': 'www.pishop.us',
-            'host': 'www.pishop.us',
+            'authority': f'www.pishop.{self.site_suffix}',
+            'host': f'www.pishop.{self.site_suffix}',
             'scheme': 'https',
             'accept': 'application/vnd.bc.v1+json',
             'accept-encoding': 'gzip, deflate',
@@ -154,8 +159,8 @@ class PiShop:
             'content-type': 'application/json',
             'X-XSRF-TOKEN': self.session.cookies.get_dict()['XSRF-TOKEN'],
             'x-checkout-sdk-version': '1.269.0',
-            'referer': "https://www.pishop.us/checkout",
-            'origin': 'https://www.pishop.us',
+            'referer': f"https://www.pishop.{self.site_suffix}/checkout",
+            'origin': f'https://www.pishop.{self.site_suffix}',
             'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
             'sec-ch-ua-mobile': '?0',
@@ -171,14 +176,14 @@ class PiShop:
 
     def submit_shipping(self):
         shipping_headers = {
-            'host': 'www.pishop.us',
+            'host': f'www.pishop.{self.site_suffix}',
             'scheme': 'https',
             'accept': 'application/json',
             'accept-encoding': 'gzip, deflate',
             'accept-language': 'en-US,en;q=0.9',
             'content-type': 'application/json',
             'X-XSRF-TOKEN': self.session.cookies.get_dict()['XSRF-TOKEN'],
-            'origin': 'https://www.pishop.us',
+            'origin': f'https://www.pishop.{self.site_suffix}',
             'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
             'sec-ch-ua-mobile': '?0',
@@ -190,7 +195,7 @@ class PiShop:
             'Connection': 'keep-alive',
             'Cookie': self.request_cookies()}
 
-        cosignment_url = f'https://www.pishop.us/api/storefront/checkouts/{self.cart_id}/consignments?include=consignments.availableShippingOptions%2Ccart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners%2C0'
+        cosignment_url = f'https://www.pishop.{self.site_suffix}/api/storefront/checkouts/{self.cart_id}/consignments?include=consignments.availableShippingOptions%2Ccart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners%2C0'
         shipping_data = [{"address": {
             "firstName": self.profile['shipping_fname'],
             "lastName": self.profile['shipping_lname'],
@@ -221,7 +226,7 @@ class PiShop:
         # Uses the cosignment ID to obtain shipping method needed.
         # Can make slightly faster by hard coding cheapest Fedex rate but for now fetches rate to avoid errors
         self.status_signal.emit({"msg": "Submitting shipping rate", "status": "normal"})
-        url_to_post = f'https://www.pishop.us/api/storefront/checkouts/{self.cart_id}/consignments/{self.cosignment_id}?include=consignments.availableShippingOptions%2Ccart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
+        url_to_post = f'https://www.pishop.{self.site_suffix}/api/storefront/checkouts/{self.cart_id}/consignments/{self.cosignment_id}?include=consignments.availableShippingOptions%2Ccart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
         shipping_rate = {"shippingOptionId": shipping_rate}
         self.status_signal.emit({"msg": "Submitting shipping rate", "status": "normal"})
         rate_submit = self.session.put(url_to_post, json=shipping_rate, headers=shipping_headers)
@@ -229,12 +234,12 @@ class PiShop:
         # Submits the billing address for the order
         self.status_signal.emit({"msg": "Submitting billing", "status": "normal"})
         billing_id = rate_submit.json()['billingAddress']['id']
-        billing_url = f'https://www.pishop.us/api/storefront/checkouts/{self.cart_id}/billing-address/{billing_id}?include=cart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
+        billing_url = f'https://www.pishop.{self.site_suffix}/api/storefront/checkouts/{self.cart_id}/billing-address/{billing_id}?include=cart.lineItems.physicalItems.options%2Ccart.lineItems.digitalItems.options%2Ccustomer%2Cpromotions.banners'
         self.session.put(billing_url, json=billing_data, headers=shipping_headers)
 
     def submit_captcha(self):
         spam_protection = {
-            'host': 'www.pishop.us',
+            'host': f'www.pishop.{self.site_suffix}',
             'method': 'POST',
             'scheme': 'https',
             'accept': 'application/json',
@@ -245,8 +250,8 @@ class PiShop:
             'X-Checkout-SDK-Version': '1.269.0',
             'Content-Length': '95',
             'path': f'/api/storefront/checkouts/{self.cart_id}/spam-protection',
-            'origin': 'https://www.pishop.us',
-            'referrer': 'https://www.pishop.us/checkout',
+            'origin': f'https://www.pishop.{self.site_suffix}',
+            'referrer': f'https://www.pishop.{self.site_suffix}/checkout',
             'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
             'sec-ch-ua-mobile': '?0',
@@ -267,12 +272,12 @@ class PiShop:
         else:
             cap_data = {'token': captcha}
             self.status_signal.emit({"msg": "Submitting captcha", "status": "normal"})
-            cap_submit = self.session.post(f'https://www.pishop.us/api/storefront/checkouts/{self.cart_id}/spam-protection', json=cap_data,
+            cap_submit = self.session.post(f'https://www.pishop.{self.site_suffix}/api/storefront/checkouts/{self.cart_id}/spam-protection', json=cap_data,
                                 headers=spam_protection)
 
     def submit_payment_info(self):
         payment_headers = {
-            'host': 'www.pishop.us',
+            'host': f'www.pishop.{self.site_suffix}',
             'method': 'POST',
             'scheme': 'https',
             'accept': 'application/json',
@@ -283,8 +288,8 @@ class PiShop:
             'X-Checkout-SDK-Version': '1.269.0',
             'Content-Length': '95',
             'path': '/internalapi/v1/checkout/order',
-            'origin': 'https://www.pishop.us',
-            'referrer': 'https://www.pishop.us/checkout',
+            'origin': f'https://www.pishop.{self.site_suffix}',
+            'referrer': f'https://www.pishop.{self.site_suffix}/checkout',
             'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
             'sec-ch-ua-mobile': '?0',
@@ -302,7 +307,7 @@ class PiShop:
         check_data = {'path': f'/checkout?cartId={self.cart_id}',
                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                       'Cookie': payment_headers['Cookie']}
-        token_get = self.session.get(f'https://www.pishop.us/checkout?cartId={self.cart_id}', allow_redirects=True, headers=check_data)
+        token_get = self.session.get(f'https://www.pishop.{self.site_suffix}/checkout?cartId={self.cart_id}', allow_redirects=True, headers=check_data)
         decoded_token = get_varIdentifyToken(token_get.text)
 
         # Appends checkout token to headers
@@ -313,45 +318,49 @@ class PiShop:
         self.status_signal.emit({"msg": "Submitting Order", "status": "alt"})
 
         # We submit the order now
-        paying = self.session.post('https://www.pishop.us/internalapi/v1/checkout/order', headers=payment_headers, json=checkout_data, allow_redirects=False).json()
+        paying = self.session.post(f'https://www.pishop.{self.site_suffix}/internalapi/v1/checkout/order', headers=payment_headers, json=checkout_data, allow_redirects=False).json()
+        end_time = time.time() - self.start_time
         if 'data' in paying:
             price = str(float(int(paying['data']['order']['grandTotal']['integerAmount'])/100))
             if 'AWAITING_PAYMENT' in paying['data']['order']['status']:
                 self.status_signal.emit({"msg": "Successful Checkout", "status": "success"})
                 if self.settings['webhooksuccess']:
-                    good_web(self.info, self.image,'PiShop (US)',self.title,self.profile['profile_name'],price)
+                    new_web('success', f"PiShop ({self.site_suffix.upper()})", self.image, self.title,
+                            self.profile['profile_name'], price=price, checkout_time=end_time)
                 if self.settings['notifsuccess']:
                     send_notif(self.title,'success')
             else:
                 self.status_signal.emit({"msg": f"Checkout Failed[{paying['data']['order']['status']}]", "status": "error"})
                 if self.settings['webhookfailed']:
-                    failed_web(self.info, self.image,'PiShop (US)',self.title,self.profile['profile_name'],price)
+                    new_web('failed', f"PiShop ({self.site_suffix.upper()})", self.image, self.title,
+                            self.profile['profile_name'], price=price, checkout_time=end_time)
                 if self.settings['notiffailed']:
                     send_notif(self.title,'fail')
     def handle_captcha(self):
         sitekey = '6LcjX0sbAAAAACp92-MNpx66FT4pbIWh-FTDmkkz'
         self.status_signal.emit({"msg": f"Awaiting Captcha ({self.captcha_type})", "status": "alt"})
+        domain = f'www.pishop.{self.site_suffix}'
         if self.captcha_type == 'Manual Harvester':
             if self.settings['notifcaptcha']:
                 send_notif(self.title,'captcha')
             try:
-                from app import MainWindow
-                if not MainWindow.pishop_started:
-                    MainWindow.pishop_started = True
-                    MainWindow.pishop_harvester.launch_browser()
+                pishop_harvester = Harvester('127.0.0.1', 5001 if self.site_suffix == 'us' else 5002)
+                pishop_token = pishop_harvester.intercept_recaptcha_v2(str(domain), str(sitekey))
+                pishop_thread = Thread(target=pishop_harvester.serve, daemon=True)
+                pishop_thread.start()
+                pishop_harvester.launch_browser()
                 while True:
                     # block until we get sent a captcha token and repeat
-                    captcha = MainWindow.pishop_harvester.get_token_queue('www.pishop.us').get()
+                    captcha = pishop_token.get()
                     break
-                MainWindow.pishop_started = False
                 return captcha
             except KeyboardInterrupt:
                 pass
 
         elif self.captcha_type == 'CapMonster':
-            return utils.get_captcha_cap('https://www.pishop.us',sitekey)
+            return utils.get_captcha_cap(f'https://www.pishop.{self.site_suffix}',sitekey)
         elif self.captcha_type == '2Captcha':
-            return utils.get_captcha_two('https://www.pishop.us',sitekey)
+            return utils.get_captcha_two(f'https://www.pishop.{self.site_suffix}',sitekey)
 
     def request_cookies(self):
         cook_string = ''
